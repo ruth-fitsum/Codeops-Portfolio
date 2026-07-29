24db38832c8c29d0f19fbec033c6c2323a62e9c9
Ruth <ruth.endeshaw.fitsum@gmail.com>
2026-07-29 14:20:54 +0300

    feat:add solution for Module 01 Day5 exercise level 2

---
+++ Exercises/day05_exercises/day5_level2.py
@@ -0,0 +1,24 @@
+# Level 2: Intermediate
+
+# 4, Method Overriding
+# done on the mini project Day 5 
+
+# 5, Polymorphism Practice
+
+from Mini_Project.Addis_Bank_Account_management_system.account import Account,SavingsAccount,CurrentAccount
+
+account1 = Account("Ruth", 1001, 5000)
+savings1 = SavingsAccount("Abebe", 1002, 10000, 5)
+current1 = CurrentAccount("Mekdes", 1003, 15000, 5000)
+
+accounts_list = [
+    account1,
+    savings1,
+    current1
+]
+
+for account in accounts_list:
+    print(account.statement())
+    account.deposit(100)
+
+# 6, Abstract Base Class - will be done on Mini Project 
