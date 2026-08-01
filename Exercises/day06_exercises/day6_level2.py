dcb3aec97b7428db42cb5a50cb99970d72e28aef
Ruth <ruth.endeshaw.fitsum@gmail.com>
2026-08-02 01:07:26 +0300

    fix:add DB_Saving class

--- Exercises/day06_exercises/day6_level2.py
+++ Exercises/day06_exercises/day6_level2.py
@@ -18,17 +18,19 @@
 # will take a snippet from the Mini project 
 # I made notifiers a list where there could be more than one notifiers
 class Account:
-    def __init__(self,name,number,balance,notifiers):
+    def __init__(self,name,number,balance,notifiers,db):
         self.name=name
         self.number=number
         self.balance=balance
         self.notifiers=notifiers
+        self.db=db
     def deposit(self,amount):
         if amount <=0:
             raise ValueError("Amount has be greater than zero.")
         self.balance+=amount
         for notifier in self.notifiers:
             notifier.send(f"{self.name} deposited {amount} ETB")
+        self.db.save_to_do()
 
     def withdraw(self,amount):
         if amount <= 0:
@@ -38,6 +40,7 @@ class Account:
         self.balance -= amount
         for notifier in self.notifiers:
             notifier.send(f"{self.name} withdrew {amount} ETB")
+        self.db.save_to_do()
 
 
 class EmailNotifier:
@@ -51,6 +54,12 @@ class SMSNotifier:
 class AuditLog:
     def send(self,message):
         print(f"Audit:{message}")
+
+class DB_Saving:
+    def save_to_do(self):
+        print("Saving to database")
+
+# so for observers and such class we need to have an instance at first and when we need to create that particular we pass them that observers's or IDK what you call AccountRepository's object
 
 # 2, Factory Pattern 
 
