37178fe9da4ee42537742ae92f6833984dafe4a7
Ruth <ruth.endeshaw.fitsum@gmail.com>
2026-08-15 20:31:15 +0300

    feat:final change for day 9 based on exercise

--- Module01/Mini_Project/Addis_Bank_Account_management_system/account.py
+++ Module01/Mini_Project/Addis_Bank_Account_management_system/account.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from abc import ABC,abstractmethod
+from collections import deque
+import heapq
+
 
 # based on Day 5 let's make it Abstract class 
-class Account(ABC):
+class Account():
     #created_date is not passed by user but automatically generated 
     def __init__(self,name,number,balance):
         #after some taught the other attributes also need to be private cause they are facts 
@@ -38,6 +41,9 @@ class Account(ABC):
     @property 
     def subscribers(self):
         return self._observers
+
+    def get_history(self):
+        return self.__history
     
     def deposit(self,amount):
         if amount <=0:
@@ -53,11 +59,11 @@ class Account(ABC):
             raise ValueError("Amount has to be greater than zero.")
         if self.__balance < amount:
             raise ValueError("Insufficient balance")
-        self.__balance -= amount
+        self._Account__balance -= amount
         self._notify(f"-{amount} ETB")
         # Day 7 push to transaction
         # Day 8 add time 
-        self.__history.append(("withdraw", amount,datetime.now()))
+        self._Account__history.append(("withdraw", amount,datetime.now()))
     #based on day05_exercises 
     # @abstractmethod
     # def calculate_interest(self):
@@ -89,7 +95,7 @@ Created Date:   {self.created_date:%Y-%m-%d}
     # Day 7 undo last transcation
     def undo_last(self):
         transaction=self.__history.pop()
-        transaction_type,amount=transaction
+        transaction_type,amount,transaction_date=transaction
 
         if transaction_type=="deposit":
             self.__balance -=amount
@@ -109,7 +115,7 @@ class BankConfig :
            cls._instance=super().__new__(cls)
            # instance is the instance hahaha
            # and know we are giving some attributes for the instance 
-           cls._instance.interest_rate =0.05
+           cls._instance.interest_rate =5
            cls._instance.overdraft_limit=1000
         return cls._instance # with everything we are setting it on
 
@@ -161,8 +167,8 @@ class SavingsAccount(Account,InterestBearing):
 # second level of inheritance - Fixed Deposit account
 
 class FixedDepositAccount(SavingsAccount):
-    def __init__(self,name,number,balance,rate,term):
-        super().__init__(name,number,balance,rate)
+    def __init__(self,name,number,balance,term):
+        super().__init__(name,number,balance)
         self.__term=term
 
     @property 
@@ -205,10 +211,11 @@ class CurrentAccount(Account):
         # one point from withdraw
         if amount <=0:
             raise ValueError("Withdraw amount has to be grater than zero")
-        if amount > self.__balance+self.__overdraft:
+        if amount > self.balance+self.__overdraft:
             raise ValueError("Overdraft limit exceeded.")
-        self._balance -= amount
-        self._notify(f"{amount} ETB")
+        self._Account__balance -= amount
+        self._notify(f"-{amount} ETB")
+        self._Account__history.append(("withdraw", amount, datetime.now()))
           
     # overriding statement 
     def statement(self):
@@ -263,94 +270,66 @@ class AuditLog:
     def update(self,message):
             print(f"Audit: {message}")
     
-#Store all accounts in a dictionary
-accounts={} # account_number → account object
 
-running =True
-while running:
-    print("""
-========== ADDIS BANK ==========
-
-1.Create new account
-2.Deposit
-3.Withdraw
-4.Check balance
-5.View account info
-6.Exit
-=================================
-""")
-    
-    choice=input("Enter your choice: ")
-try :
-    if choice =="1":
-        name=input("Enter customer's name: ")
-        initial_balance=int(input("Enter inital balance: "))
-        account_number=int(input("Enter the account number: "))
-        # Day 7 search an account is there or not 
-        if account_number in accounts:
-            print("Account number already exists.")
-        else:
-            account=Account(name,account_number,initial_balance)
-            accounts[account_number]=account
-            print("Account created successfully.")
-
-    elif choice=="2":
-        account_number=int(input("Enter account number: "))
-        amount=int(input("Enter deposit amount: "))
-        account=accounts[account_number]
-        account.deposit(amount)
-
-    elif choice =="3":
-        account_number=int(input("Enter account number: "))
-        amount=int(input("Enter withdrawal amount: "))
-        account=accounts[account_number]
-        account.withdraw(amount)
-
-    elif choice =="4":
-        # Day 7 search by account number 
-        account_number=int(input("Enter account number: "))
-        account=accounts[account_number]
-        print(f"{account.balance} ETB")
-
-    elif choice=="5":
-        account_number=int(input("Enter account number: "))
-        account=accounts[account_number]
-        # I prefer __str__()
-        print(account)
-
-    elif choice=="6":
-        print("Thank you for using Addis Bank.")
-        running=False
-    else:
-        print("Please select only from 1-6.")
-# for every user entry
-except ValueError as error:
-    print(f"Error: {error}")
-# when the user wants to change or update an account that doesn't exist
-except KeyError:
-    print("Account not found.")
 
 # Day 7
+# Day 8 - Binary Search for account numbers
+def binary_search_accounts(numbers, target):
+    left = 0
+    right = len(numbers) - 1
 
+    while left <= right:
+        middle = (left + right) // 2
+
+        if numbers[middle] == target:
+            return middle
+
+        elif numbers[middle] < target:
+            left = middle + 1
+
+        else:
+            right = middle - 1
+
+    return -1
+
+
+# Day 7 & Day 8
 class AccountRegistry:
     def __init__(self):
-        self.by_number={} # for fast look up 
-        self.order=[] # for ordering 
-    def add(self,acc):
-        self.by_number[acc.number]=acc
+        self.by_number = {}   # Fast lookup by account number
+        self.order = []       # Keeps insertion order
+
+    def add(self, acc):
+        self.by_number[acc.number] = acc
         self.order.append(acc.number)
-    def find(self,number):
+
+    def find(self, number):
         return self.by_number.get(number)
+
     def list_all(self):
-        return self.order 
-    # Day 8
+        return self.order
+
+    # Day 8 - Top accounts by balance
     def top_by_balance(self, n=5):
-        accts = sorted(self.by_number.values(),key=lambda a: a.balance,reverse=True)
+        accts = sorted(
+            self.by_number.values(),
+            key=lambda a: a.balance,
+            reverse=True
+        )
         return accts[:n]
+
+    # Day 8 - Binary search for account number
     def find_by_number(self, number):
         nums = sorted(self.by_number)
-        i = binary_search_transactions(nums, number)
-        return self.by_number[nums[i]] if i >= 0 else None
+
+        index = binary_search_accounts(nums, number)
+
+        if index >= 0:
+            return self.by_number[nums[index]]
+
+        return None
+
+    # Day 8 - Recursive total transactions
     def total_transactions(self, number):
         account = self.find(number)
 
@@ -366,30 +345,399 @@ class AccountRegistry:
             return history[index][1] + sum_history(index + 1)
 
         return sum_history(0)
-    # Day 8 - need to have sorted history 
+
+    # Day 8 - Sort transactions by date
     def sort_transactions_by_date(self, number):
         account = self.find(number)
 
         if account is None:
             return []
 
-        return sorted(account.get_history(),key=lambda transaction: transaction[2])
+        return sorted(
+            account.get_history(),
+            key=lambda transaction: transaction[2]
+        )
 
-# Day 8 - binary search
-def binary_search_transactions(self, number, target_date):
-    account = self.find(number)
-    if account is None:
+    # Day 8 - Binary search transaction by date
+    def binary_search_transactions(self, number, target_date):
+        account = self.find(number)
+
+        if account is None:
+            return None
+
+        transactions = sorted(
+            account.get_history(),
+            key=lambda transaction: transaction[2]
+        )
+
+        left = 0
+        right = len(transactions) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+
+            transaction_date = transactions[middle][2]
+
+            if transaction_date == target_date:
+                return transactions[middle]
+
+            elif transaction_date < target_date:
+                left = middle + 1
+
+            else:
+                right = middle - 1
+
         return None
-    transactions = sorted(account.get_history(),key=lambda transaction: transaction[2])
-    left = 0
-    right = len(transactions) - 1
-    while left <= right:
-        middle = (left + right) // 2
-        transaction_date = transactions[middle][2]
-        if transaction_date == target_date:
-            return transactions[middle]
-        elif transaction_date < target_date:
-            left = middle + 1
+ 
+# Day 9 
+class Branch:
+    def __init__(self,name):
+        self.name=name
+        self.children=[]
+        self.accounts=[]
+        self.employees = []
+    # based on Day 9 exercises  
+    def add_branch(self, branch):
+        self.children.append(branch)
+    def add_employee(self, employee):
+        self.employees.append(employee)
+    def total_balance(self):
+        total=sum(a.balance for a in self .accounts)
+        for child in self.children:
+            total +=child.total_balance()
+        return total
+# transfer graph
+
+class TransferGraph:
+    def __init__(self):
+        self.transfers = {}
+
+    def add_transfer(self, sender, recipient):
+        if sender not in self.transfers:
+            self.transfers[sender] = []
+
+        self.transfers[sender].append(recipient)
+
+    def bfs(self, start):
+        visited = set()
+        queue = deque([start])
+
+        while queue:
+            account = queue.popleft()
+
+            if account in visited:
+                continue
+
+            visited.add(account)
+
+            for recipient in self.transfers.get(account, []):
+                if recipient not in visited:
+                    queue.append(recipient)
+
+        return visited
+
+    # Let's add a DFS based on day 9 exercise 
+    def dfs(self, start, visited=None):
+        if visited is None:
+            visited = set()
+
+        if start in visited:
+            return visited
+
+        visited.add(start)
+
+        for recipient in self.transfers.get(start, []):
+            self.dfs(recipient, visited)
+
+        return visited
+
+# Urgent transactions 
+# Time Complexity: O(log n)
+urgent_transactions = []   
+def add_urgent_transaction(sender, recipient, amount, priority):
+    transaction = (priority, sender, recipient, amount)
+
+    heapq.heappush(urgent_transactions, transaction)
+
+    print("Urgent transaction added successfully.")
+
+# O(log n)
+# process highest priority transaction 
+def process_urgent_transaction():
+    if not urgent_transactions:
+        print("No urgent transactions to process.")
+        return
+
+    transaction = heapq.heappop(urgent_transactions)
+
+    priority, sender, recipient, amount = transaction
+
+    print("Processing urgent transaction...")
+    print(f"Priority: {priority}")
+    print(f"Sender: {sender}")
+    print(f"Recipient: {recipient}")
+    print(f"Amount: {amount} ETB")
+
+# Search for customer account in BST
+# Worst-case Time Complexity: O(n)
+
+class AccountNode:
+    def __init__(self, account):
+        self.account = account
+        self.left = None
+        self.right = None
+
+
+class AccountBST:
+    def __init__(self):
+        self.root = None
+
+    # Add account to BST
+    def insert(self, account):
+        new_node = AccountNode(account)
+
+        if self.root is None:
+            self.root = new_node
+            return
+
+        current = self.root
+
+        while True:
+            if account.number < current.account.number:
+
+                if current.left is None:
+                    current.left = new_node
+                    return
+
+                current = current.left
+
+            elif account.number > current.account.number:
+
+                if current.right is None:
+                    current.right = new_node
+                    return
+
+                current = current.right
+
+            else:
+                print("Account number already exists.")
+                return
+
+    # Search for customer account
+    def search(self, account_number):
+        current = self.root
+
+        while current is not None:
+
+            if account_number == current.account.number:
+                return current.account
+
+            elif account_number < current.account.number:
+                current = current.left
+
+            else:
+                current = current.right
+
+        return None
+
+# Store all accounts in a dictionary
+accounts = {}
+
+# Day 9 data structures
+root_branch = Branch("Main Branch")
+transfer_graph = TransferGraph()
+account_bst = AccountBST()
+
+running = True
+
+while running:
+
+    print("""
+========== ADDIS BANK ==========
+
+1. Create new account
+2. Deposit
+3. Withdraw
+4. Check balance
+5. View account info
+6. Add new branch / employee
+7. Add money transfer connection
+8. Show all connected customers using BFS/DFS
+9. Add urgent transaction
+10. Process highest priority transaction
+11. Search for customer account in BST
+12. Exit
+
+=================================
+""")
+    choice = input("Enter your choice: ")
+    try:
+        if choice == "1":
+
+            name = input("Enter customer's name: ")
+            initial_balance = int(input("Enter initial balance: "))
+            account_number = int(input("Enter account number: "))
+
+            if account_number in accounts:
+                print("Account number already exists.")
+
+            else:
+                account = Account(
+                    name,
+                    account_number,
+                    initial_balance
+                )
+                # Store in dictionary
+                accounts[account_number] = account
+                # Also store in BST
+                account_bst.insert(account)
+                print("Account created successfully.")
+        elif choice == "2":
+            account_number = int(
+                input("Enter account number: ")
+            )
+            amount = int(
+                input("Enter deposit amount: ")
+            )
+            account = accounts[account_number]
+            account.deposit(amount)
+            print("Deposit successful.")
+        elif choice == "3":
+            account_number = int(
+                input("Enter account number: ")
+            )
+            amount = int(
+                input("Enter withdrawal amount: ")
+            )
+            account = accounts[account_number]
+            account.withdraw(amount)
+            print("Withdrawal successful.")
+        elif choice == "4":
+            account_number = int(
+                input("Enter account number: ")
+            )
+            account = accounts[account_number]
+            print(f"Balance: {account.balance:,} ETB")
+        elif choice == "5":
+            account_number = int(
+                input("Enter account number: ")
+            )
+            account = accounts[account_number]
+            print(account)
+        elif choice == "6":
+            branch_name = input(
+                "Enter branch name: "
+            )
+            new_branch = Branch(branch_name)
+            root_branch.add_branch(new_branch)
+            employee = input(
+                "Enter employee name: "
+            )
+            new_branch.add_employee(employee)
+            print(
+                "Branch and employee added successfully."
+            )        
+        elif choice == "7":
+            sender = int(
+                input("Enter sender account number: ")
+            )
+            if sender not in accounts:
+                print("Sender account not found.")
+                continue
+            recipient = int(
+                input("Enter recipient account number: ")
+            )
+            if recipient not in accounts:
+                print("Recipient account not found.")
+                continue
+            transfer_graph.add_transfer(
+                sender,
+                recipient
+            )
+            print(
+                "Money transfer connection added successfully."
+            )
+        elif choice == "8":
+            start = int(
+                input("Enter starting customer account: ")
+            )
+            if start not in accounts:
+                print("Account not found.")
+                continue
+            method = input(
+                "Choose BFS or DFS: "
+            ).upper()
+            if method == "BFS":
+                connected = transfer_graph.bfs(start)
+                print(
+                    "BFS connected customers:",
+                    connected
+                )
+            elif method == "DFS":
+
+                connected = transfer_graph.dfs(start)
+                print(
+                    "DFS connected customers:",
+                    connected
+                )
+            else:
+                print(
+                    "Please choose BFS or DFS."
+                )
+        elif choice == "9":
+
+            sender = int(
+                input("Enter sender account number: ")
+            )
+            if sender not in accounts:
+                print("Sender account not found.")
+                continue
+
+            recipient = int(
+                input("Enter recipient account number: ")
+            )
+
+            if recipient not in accounts:
+                print("Recipient account not found.")
+                continue
+            amount = int(
+                input("Enter transaction amount: ")
+            )
+
+            priority = int(
+                input("Enter priority: ")
+            )
+
+            add_urgent_transaction(
+                sender,
+                recipient,
+                amount,
+                priority
+            )
+        elif choice == "10":
+            process_urgent_transaction()
+        elif choice == "11":
+            account_number = int(
+                input("Enter account number to search: ")
+            )
+            account = account_bst.search(
+                account_number
+            )
+            if account:
+                print("Account found!")
+                print(account)
+            else:
+                print("Account not found.")
+        elif choice == "12":
+            print(
+                "Thank you for using Addis Bank."
+            )
+            running = False
         else:
-            right = middle - 1
-    return None
+            print(
+                "Please select only from 1-12."
+            )
+    except ValueError as error:
+        print(f"Error: {error}")
+    except KeyError:
+        print("Account not found.")
